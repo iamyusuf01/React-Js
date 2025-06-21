f0be9549dc3970012ae02bf1ce3e842e46232ff0
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-06-22 00:09:38 +0530

    MegaBlog Web App

---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import Logout from "./Header/Logout"
+import Button from './Button'
+import Input from './Input'
+import RTE from "./RTE"
+import Select from "./Select"
+import Signup from './Signup'
+import Login from "./Login"
+
+import PostFrom from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Logout,
+    Button,
+    Input,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostFrom,
+    PostCard,
+    AuthLayout
+}
